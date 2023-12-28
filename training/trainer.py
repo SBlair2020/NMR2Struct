cbb65ba89a4a72b01539c5fcf1d2a095b44eb8b8
Frank Hu <frankhu@markland-gpu.stanford.edu>
2023-12-27 16:46:31 -0800

    Fleshed out train, validation, and test loops, and added __init__ file

--- training/trainer.py
+++ training/trainer.py
@@ -2,34 +2,99 @@ import numpy as np
 import pickle, os, shutil
 import torch
 import torch.nn as nn
-#Project structural imports
-from trainer import NMRML
-from Models import NMRCNNModel, subs_weighted_BCE
-from data import SpectraHDF5Dataset
+from torch import Tensor
+from typing import Callable, Optional
+from torch.utils.tensorboard import SummaryWriter
 
-# from nmr.networks import NMRConvNet
-
-def train_loop(get_loss, model, loss, optimizer, scheduler, epoch, write_freq=100):
+def train_loop(model: nn.Module, 
+               dataloader: torch.utils.data.DataLoader, 
+               loss_fn: Callable[[Tensor, Tensor], Tensor], 
+               optimizer: torch.optim.Optimizer, 
+               epoch: int, writer: torch.utils.tensorboard.SummaryWriter, 
+               scheduler: Optional[torch.optim.lr_scheduler.LambdaLR], 
+               write_freq: int = 100):
+    """Model training loop
+    Args:
+        model: The model to train
+        dataloader: The dataloader for the training dataset
+        loss_fn: The loss function to use for the model, with the signature
+            tensor, tensor -> tensor
+        optimizer: The optimizer for training the model
+        epoch: The current epoch
+        writer: Tensorboard writer for logging losses and learning rates
+        scheduler: The optional learning rate scheduler
+        write_freq: The frequency for printing loss information
+    """
     tot_loss = 0
+    model.train()
     for ibatch, (x, y) in enumerate(dataloader):
         inner_step = int(( epoch * len(dataloader)) + ibatch)
-        loss = get_loss(x,y,model,loss) # TODO make this a class method
+        loss = model.get_loss(x,y,loss_fn) 
         optimizer.zero_grad()
         loss.backward()
-        #self.writer.add_scalar("Step Learning Rate", self.optimizer.param_groups[0]['lr'], inner_step)
         optimizer.step()
         #Step the learning rate scheduler too based on the current optimizer step
         if scheduler is not None:
             scheduler.step()
-        if (ibatch % 100 == 99):
+        if (ibatch % write_freq == 0):
             print(f"Epoch: {epoch}\tBatch:{ibatch}\tTrain Loss:{loss.item()}")
         tot_loss += loss.item()
-        #self.writer.add_scalar("Training Step Loss", loss.item(), inner_step)
-    #self.writer.add_scalar("Avg. Epoch Train Loss", tot_loss / len(self.train_loader), epoch)
+        writer.add_scalar("Training Step Loss", loss.item(), inner_step)
+    writer.add_scalar("Avg. Epoch Train Loss", tot_loss / len(dataloader), epoch)
     return tot_loss / len(dataloader)
 
-def validation_loop():
-    return NotImplementedError
+def validation_loop(model: nn.Module, 
+                    dataloader: torch.utils.data.DataLoader, 
+                    loss_fn: Callable[[Tensor, Tensor], Tensor], 
+                    epoch: int, writer: torch.utils.tensorboard.SummaryWriter, 
+                    write_freq: int = 100):
+    """Model validation loop
+    Args:
+        model: The model to validate
+        dataloader: The dataloader for the validation dataset
+        loss_fn: The loss function to use for the model, with the signature
+            tensor, tensor -> tensor
+        epoch: The current epoch
+        writer: Tensorboard writer for logging losses and learning rates
+        write_freq: The frequency for printing loss information
+    """
+    tot_loss = 0
+    model.eval()
+    with torch.no_grad():
+        for ibatch, (x, y) in enumerate(dataloader):
+            inner_step = int(( epoch * len(dataloader)) + ibatch)
+            loss = model.get_loss(x,y,loss_fn) 
+            if (ibatch % write_freq == 0):
+                print(f"Epoch: {epoch}\tBatch:{ibatch}\tValidation Loss:{loss.item()}")
+            tot_loss += loss.item()
+            writer.add_scalar("Validation Step Loss", loss.item(), inner_step)
+        writer.add_scalar("Avg. Epoch Validation Loss", tot_loss / len(dataloader), epoch)
+    return tot_loss / len(dataloader)
 
-def test_loop():
-    return NotImplementedError
+def test_loop(model: nn.Module, 
+                dataloader: torch.utils.data.DataLoader, 
+                loss_fn: Callable[[Tensor, Tensor], Tensor], 
+                epoch: int, writer: torch.utils.tensorboard.SummaryWriter, 
+                write_freq: int = 100):
+    """Model test loop
+    Args:
+        model: The model to test
+        dataloader: The dataloader for the test dataset
+        loss_fn: The loss function to use for the model, with the signature
+            tensor, tensor -> tensor
+        epoch: The current epoch
+        writer: Tensorboard writer for logging losses and learning rates
+        write_freq: The frequency for printing loss information
+    """
+    tot_loss = 0
+    model.eval()
+    with torch.no_grad():
+        for ibatch, (x, y) in enumerate(dataloader):
+            inner_step = int(( epoch * len(dataloader)) + ibatch)
+            loss = model.get_loss(x,y,loss_fn) 
+            if (ibatch % write_freq == 0):
+                print(f"Epoch: {epoch}\tBatch:{ibatch}\Test Loss:{loss.item()}")
+            tot_loss += loss.item()
+            writer.add_scalar("Test Step Loss", loss.item(), inner_step)
+        writer.add_scalar("Avg. Epoch Test Loss", tot_loss / len(dataloader), epoch)
+    return tot_loss / len(dataloader)
